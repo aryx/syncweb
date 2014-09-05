e68efe953e13e91353ea1603251b3c4c59765b88
pad <pad@fb.com>
2014-09-04 17:37:23 -0700

    * engine.ml: use .md5sum_foo_c not .md5sum_foo.c, better for codemap and mkmany

--- engine.ml
+++ engine.ml
@@ -72,7 +72,13 @@ let re_md5sum_in_aux_file = Str.regexp
 
 let md5sum_auxfile_of_file file = 
   let (d,b) = Common2.db_of_filename file in
-  Common2.filename_of_db (d, ".md5sum_" ^ b)
+  let oldformat = Common2.filename_of_db (d, ".md5sum_" ^ b) in
+  if Sys.file_exists oldformat
+  then oldformat
+  else 
+    let (d,b,e) = Common2.dbe_of_filename file in
+    (* works better with codemap, and also mkmany in plan9 *)
+    Common2.filename_of_dbe (d, spf ".md5sum_%s_%s" b e, "")
 
 (*****************************************************************************)
 (* Helpers  *)
