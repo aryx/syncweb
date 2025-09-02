2e69afcddfaef6066d880da1720c712ee0243a53
pad <yoann.padioleau@gmail.com>
2025-09-02 16:17:50 +0200

    Split indexer/Main.ml in CLI_indexer.ml for ease of e2e testing

---
+++ indexer/CLI_indexer.ml
@@ -0,0 +1,81 @@
+(* Copyright 2009-2017, 2025 Yoann Padioleau, see copyright.txt *)
+open Common
+
+(*****************************************************************************)
+(* Purpose *)
+(*****************************************************************************)
+(* Generate somes defs_and_uses.list file used by syncweb to automatically
+ * add crossrefs to entities in a literate document.
+ *)
+
+(*****************************************************************************)
+(* Flags *)
+(*****************************************************************************)
+
+(* possible values: "cmt", "ml" (see Index.ml) *)
+let lang = ref "c"
+
+(* action mode *)
+let action = ref ""
+
+(*****************************************************************************)
+(* Main action *)
+(*****************************************************************************)
+
+let main_action (root : string) : unit =
+  Index.build_graph_code !lang (Fpath.v root)
+
+(*****************************************************************************)
+(* The options *)
+(*****************************************************************************)
+
+let actions () = [
+]
+
+let options () = [
+  "-lang", Arg.Set_string lang, 
+  (spf " <str> choose language (default = %s)" !lang);
+  ] @
+  Arg_.options_of_actions action (actions()) @
+  []
+
+(*****************************************************************************)
+(* Main entry point *)
+(*****************************************************************************)
+
+let main (argv : string array) : unit = 
+
+  let usage_msg = 
+    "Usage: " ^ Filename.basename argv.(0) ^ 
+      " [options] -lang <lang> <dir> " ^ "\n" ^ "Options are:"
+  in
+  (* does side effect on many global flags *)
+  let args = Arg_.parse_options (options()) usage_msg argv in
+
+  (* must be done after Arg.parse, because Common.profile is set by it *)
+  Profiling.profile_code "Main total" (fun () -> 
+    
+    (match args with
+        (* --------------------------------------------------------- *)
+    (* actions, useful to debug subpart *)
+    (* --------------------------------------------------------- *)
+    | xs when List.mem !action (Arg_.action_list (actions())) -> 
+        Arg_.do_action !action xs (actions())
+
+    | _ when not (String_.empty !action) -> 
+        failwith ("unrecognized action or wrong params: " ^ !action)
+
+    (* --------------------------------------------------------- *)
+    (* main entry *)
+    (* --------------------------------------------------------- *)
+    | [x] -> 
+        main_action x
+
+    (* --------------------------------------------------------- *)
+    (* empty entry *)
+    (* --------------------------------------------------------- *)
+    | [] | _::_::_ -> 
+        Arg_.usage usage_msg (options()); 
+        failwith "too few or too many arguments"
+    )
+  )
--- lpizer/Main.ml
+++ lpizer/Main.ml
@@ -1,7 +1,5 @@
 
-(* TODO: switch to Cap.main and Exit status like in xix *)
 let _ =
-   (* UCommon.main_boilerplate (fun () ->  *)
    Cap.main (fun _all_caps -> 
       CLI_lpizer.main Sys.argv;
   )
